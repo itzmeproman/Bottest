3c211a676854c606d6ec62429b8e8f6241523362
itzmeproman <atulpandey3030@gmail.com>
2023-08-27 18:24:36 +0530

    Update video_encoder_bot.py

--- video_encoder_bot.py
+++ video_encoder_bot.py
@@ -8,7 +8,7 @@ bot = telebot.TeleBot(os.environ['BOT_TOKEN'])
 # Start message
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, 'Welcome to the video encoder bot! I can encode videos in different formats and resolutions. To get started, send me a video file.')
+    bot.reply_to(message, 'Welcome to the video encoder bot! I can encode videos in different formats and resolutions. To get started, send me a video file in MKV or MP4 format.')
 
 # Add thumbnail
 @bot.message_handler(content_types=['photo'])
@@ -36,9 +36,23 @@ def encode_video(message):
     # Get the resolution
     resolution = get_default_resolution()
 
+    # Check the file extension
+    file_extension = os.path.splitext(video_file)[1]
+    if file_extension not in ['.mkv', '.mp4']:
+        bot.send_message(message.chat.id, 'Sorry, I can only encode videos in MKV or MP4 format.')
+        return
+
     # Start the encoding process
     start_time = time.time()
     ffmpeg.input(video_file).output(output_file_name, codec=codec, preset='veryfast', resolution=resolution).run()
+
+    # Send the progress to the user
+    progress = 0
+    while progress < 100:
+        progress = int(ffmpeg.progress())
+        bot.send_message(message.chat.id, 'Download progress: {}%'.format(progress))
+        time.sleep(1)
+
     encoding_time = time.time() - start_time
 
     # Send the encoded video
@@ -47,58 +61,3 @@ def encode_video(message):
     # Set the thumbnail for the user
     set_thumbnail_for_user(user_id, thumbnail)
 
-# Get help
-@bot.message_handler(commands=['help'])
-def help(message):
-    bot.reply_to(message, 'Here are the available commands:
-    /start - Start the bot
-    /thumbnail - Add a thumbnail to the next video
-    /encode - Encode a video
-    /help - Get help')
-
-
-
-# Restart the bot
-@bot.message_handler(commands=['restart'])
-def restart(message):
-    if message.from_user.id == int(os.environ['ADMIN_ID']):
-        bot.stop_bot()
-        time.sleep(1)
-        bot.start_bot()
-        bot.send_message(message.chat.id, 'The bot has been restarted')
-    else:
-        bot.send_message(message.chat.id, 'You are not authorized to use this command')
-
-# Cancel the encoding process
-@bot.message_handler(commands=['cancel'])
-def cancel(message):
-    global thumbnail
-    global user_id
-
-    # Check if the user is allowed to cancel the process
-    if message.from_user.id == int(os.environ['ADMIN_ID']) or message.chat.id == user_id:
-        # Remove the thumbnail
-        thumbnail = None
-
-        # Cancel the process
-        os.system('pkill -f ffmpeg')
-        bot.send_message(message.chat.id, 'The encoding process has been canceled')
-    else:
-        bot.send_message(message.chat.id, 'You are not authorized to use this command')
-
-# Get the download and upload speed
-@bot.message_handler(commands=['speed'])
-def speed(message):
-    # Get the download speed
-    speed = os.popen('speedtest-cli --simple').read()
-
-    # Get the upload speed
-    speed = speed.splitlines()[-1]
-
-    # Send the speed
-    bot.send_message(message.chat.id, 'Download speed: {} Upload speed: {}'.format(speed.split(' ')[0], speed.split(' ')[1]))
-
-# Set the default resolution
-def set_default_resolution(resolution):
-    global default_resolution
-    default_resolution = resolution
